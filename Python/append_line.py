6d68d8bd20276d3a0765c1f4c9a08954f16406a9
AndrewSkelton <andrew.j.skelton73@gmail.com>
2014-02-11 10:54:34 +0000

    Corrected False space in line append

--- Python/append_line.py
+++ Python/append_line.py
@@ -2,4 +2,4 @@ import fileinput
 import sys
 
 for line in fileinput.input(['./merged.gtf'], inplace=True):
-    sys.stdout.write('chr {l}'.format(l=line))
+    sys.stdout.write('chr{l}'.format(l=line))
