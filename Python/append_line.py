6a476f6d9e24cb89278240647a953dede86ef818
AndrewSkelton <andrew.j.skelton73@gmail.com>
2014-02-11 17:34:59 +0000

    end of the day

--- Python/append_line.py
+++ Python/append_line.py
@@ -1,5 +1,17 @@
 import fileinput
 import sys
 
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
 for line in fileinput.input(['./merged.gtf'], inplace=True):
-    sys.stdout.write('chr{l}'.format(l=line))
+	info = line.split()
+	var1 = info[0]
+	if is_number(var1) == True:
+		sys.stdout.write('chr{l}'.format(l=line))
+	else:
+		sys.stdout.write('{l}'.format(l=line))
