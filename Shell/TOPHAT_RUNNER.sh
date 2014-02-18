94619e09699ae3f2579f3e5002b873ee15a37dc8
AndrewSkelton <andrew.j.skelton73@gmail.com>
2014-02-18 17:08:31 +0000

    Various Cluster Scripts for RNA Pipes

--- Shell/TOPHAT_RUNNER.sh
+++ Shell/TOPHAT_RUNNER.sh
@@ -6,6 +6,6 @@
 #$ -o ~/log
 source ~/.bash_profile
 
-tophat -r $3 --phred64-quals --tmp-dir $TMPDIR -p 4 --transcriptome-index ~/WORKING_DATA/RNA_Seq/Analysis/transcriptome_index_hg19/known \
-		--b2-very-sensitive -o ~/WORKING_DATA/RNA_Seq/Analysis/Tophat/$4 -G /opt/databases/genomes/hg19/bt2_th_hg19_ucsc/genes.gtf \
+tophat -r $3 --mate-std-dev $4 --phred64-quals --tmp-dir $TMPDIR -p 4 --transcriptome-index ~/WORKING_DATA/RNA_Seq/Analysis/transcriptome_index_hg19/known \
+		--b2-very-sensitive -o ~/WORKING_DATA/RNA_Seq/Analysis/Tophat/$5 -G /opt/databases/genomes/hg19/bt2_th_hg19_ucsc/genes.gtf \
 		/opt/databases/genomes/hg19/bt2_th_hg19_ucsc/genome	$1 $2
